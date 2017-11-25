107f4826f51425832f5e4a6d00e226edd1ac2621
Rouvinen <samuli.rouvinen@cgi.com>
2017-11-25 19:35:20 +0200

    recording

---
+++ Assets/Source/ToggleActive.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleActive : MonoBehaviour {
+
+    public void activation()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
