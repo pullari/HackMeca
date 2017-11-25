6023f3b84f16b725ba32f9e5b57f8474fbec3563
Ollila <sami-pekka.ollila@cgi.com>
2017-11-25 20:25:28 +0200

    Stuff and dental stuff

---
+++ Assets/Source/Buttons/ModelsButton.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelsButton : MonoBehaviour {
+
+    public GameObject scrollView;
+    public GameObject dental;
+    public GameObject multipleDental;
+
+
+    public void SetState(int state)
+    {
+        switch (state)
+        {
+            case 8:
+                print("Why hello there good sir! Let me teach you about Trigonometry!");
+                break;
+            case 7:
+                print("Why hello there good sir! Let me teach you about Trigonometry!");
+                break;
+            case 6:
+                print("Why hello there good sir! Let me teach you about Trigonometry!");
+                break;
+            case 5:
+                print("Why hello there good sir! Let me teach you about Trigonometry!");
+                break;
+            case 4:
+                print("Hello and good day!");
+                break;
+            case 3:
+                DisableAll();
+
+                print("Whadya want?");
+                break;
+            case 2:
+                print("Grog SMASH!");
+                break;
+            case 1:
+                print("Ulg, glib, Pblblblblb");
+                break;
+            default:
+                print("Incorrect intelligence level.");
+                break;
+        }
+    }
+
+
+    public void DisableAll()
+    {
+        scrollView.SetActive(false);
+        dental.SetActive(false);
+        multipleDental.SetActive(false);
+    }
+}
