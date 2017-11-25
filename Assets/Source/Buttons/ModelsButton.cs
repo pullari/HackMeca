209a6c22c17396667feb54123b78ef28e0a8758b
Rouvinen <samuli.rouvinen@cgi.com>
2017-11-25 22:51:05 +0200

    This can be considered done

--- Assets/Source/Buttons/ModelsButton.cs
+++ Assets/Source/Buttons/ModelsButton.cs
@@ -7,6 +7,13 @@ public class ModelsButton : MonoBehaviour {
     public GameObject scrollView;
     public GameObject dental;
     public GameObject multipleDental;
+    public GameObject popup;
+    public GameObject popup2;
+    public GameObject popup3;
+    public GameObject historiikki;
+    public GameObject dataUI;
+    public GameObject ThreeDeeData;
+    public GameObject Wearabledata;
 
     CameraLookAt camlook;
 
@@ -25,21 +32,29 @@ public class ModelsButton : MonoBehaviour {
                 break;
             case 7:
                 DisableAll();
+                dental.SetActive(true);
+                Wearabledata.SetActive(true);
                 break;
             case 6:
                 DisableAll();
                 break;
             case 5:
                 DisableAll();
+                scrollView.SetActive(true);
+                dental.SetActive(true);
+                historiikki.SetActive(true);
                 break;
             case 4:
                 DisableAll();
                 multipleDental.SetActive(true);
+                ThreeDeeData.SetActive(true);
+                scrollView.SetActive(true);
                 break;
             case 3:
                 DisableAll();
                 dental.SetActive(true);
                 scrollView.SetActive(true);
+                dataUI.SetActive(true);
                 break;
             case 2:
                 break;
@@ -55,8 +70,15 @@ public class ModelsButton : MonoBehaviour {
     {
         camlook.ResetPosition();
 
+        multipleDental.SetActive(false);
+        popup.SetActive(false);
+        popup2.SetActive(false);
+        popup3.SetActive(false);
+        historiikki.SetActive(false);
+        dataUI.SetActive(false);
+        ThreeDeeData.SetActive(false);
         scrollView.SetActive(false);
         dental.SetActive(false);
-        multipleDental.SetActive(false);
+        Wearabledata.SetActive(false);
     }
 }
